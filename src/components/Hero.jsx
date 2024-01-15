15ee64e51ac8e3087a0492f60ebd0f379828d401
valnhe <vpazcastle@gmail.com>
2024-01-14 23:08:16 -0300

    Add Hero component and hero.css file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import "./hero.css"
+
+function Hero() {
+    return (
+        <header class="h-[620px] w-full pt-[30px] sm:pt-[180px] pl-[64px] lg:pl-[112px] xl:pl-[140px] text-[#121826]">
+            <h1 class="text-[64px] font-bold w-[350px] sm:w-[500px] lg:w-[600px]">Peace, nature, dream</h1>
+            <h2 class="text-[24px]">Find and book a great experience</h2>
+        </header>
+    )
+} export default Hero;
